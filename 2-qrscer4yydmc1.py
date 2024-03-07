41611b6cb715429da79dae420812b1b13777d6ea
Uwe Ziegenhagen <UweZiegenhagen@users.noreply.github.com>
2024-03-07 16:02:49 +0100

    added second diagram
    added second diagram

--- 2-qrscer4yydmc1.py
+++ 2-qrscer4yydmc1.py
@@ -7,14 +7,14 @@ import pandas as pd
 # Define parameters for the skewed normal distribution
 a = 0  # Skewness parameter
 mean = 0  # Mean of the distribution
-std_dev = 1  # Standard deviation of the distribution
+std_dev = 2  # Standard deviation of the distribution
 
 # Generate data points for the skewed normal distribution
-x = np.linspace(-4, 4, 200)  # Range of x values
+x = np.linspace(-7, 7, 500)  # Range of x values
 y = skewnorm.pdf(x, a, loc=mean, scale=std_dev)  # Skewed normal distribution
 
 #data = np.concatenate((x,y), axis=1)
-xs = pd.Series(x).round(3)
+xs = pd.Series(x).round(3)+3
 ys = pd.Series(y).round(3)
 df = pd.concat([xs, ys], axis=1, keys=['x','y'])
 
@@ -29,6 +29,6 @@ plt.show()
 
 #df.to_csv('1.csv', index=False)
 
-with open('0zq0aw4yydmc1.txt', 'w') as o:
+with open('2-qrscer4yydmc1.txt', 'w') as o:
     for index, row in df.iterrows():
         o.write('(' + str(row['x']) + ',' + str(row['y']) + ') -- '  )
