e456b0d1c8242788fb8092d63c21870c8841ffb1
Uwe Ziegenhagen <UweZiegenhagen@users.noreply.github.com>
2024-03-09 13:32:03 +0100

    created alternative plot
    created alternative plot

---
+++ 3alternative-awlj4dj9zdmc1.py
@@ -0,0 +1,32 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import skewnorm
+import pandas as pd
+
+# Define parameters for the skewed normal distribution
+a = 0 # Skewness parameter
+mean = -2  # Mean of the distribution
+std_dev = 1  # Standard deviation of the distribution
+
+# Generate data points for the skewed normal distribution
+x = np.linspace(-7, 7, 500)  # Range of x values
+y = skewnorm.pdf(x, a, loc=mean, scale=std_dev)  # Skewed normal distribution
+
+#data = np.concatenate((x,y), axis=1)
+xs = pd.Series(x).round(3)
+ys = pd.Series(y).round(3)-0.5
+df = pd.concat([xs, ys], axis=1, keys=['x','y'])
+
+
+# Plot the skewed normal distribution
+plt.plot(x, y, color='blue')
+plt.title('Skewed Normal Distribution')
+plt.xlabel('X')
+plt.ylabel('Probability Density')
+plt.grid(True)
+plt.show()
+
+with open('3alt-b.txt', 'w') as o:
+    for index, row in df.iterrows():
+        o.write('(' + str(row['x']) + ',' + str(row['y']) + ') -- '  )
